11584977fe1e263d7c08c7f7e9d82d6374077932
ManojLogiciel <manoj.kumar@logicielsolutions.co.in>
2019-09-28 09:02:24 +0530

    fixes

--- www/js/controllers/home.center.list.controller.js
+++ www/js/controllers/home.center.list.controller.js
@@ -140,18 +140,11 @@
       }
     }
 
-    function diff_hours(dt2, dt1) {
-      var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-      diff /= (60 * 60);
-      if (Math.round(diff) <= 2) {
-        return 2;
-      }
-      if (Math.round(diff) <= 4) {
-        return 4;
-      }
-      if (Math.round(diff) <= 8) {
-        return 8;
-      }
+    function diff_hours(dt1, dt2) {
+      var diff = Math.abs(dt1 - dt2) / 36e5;
+      if (diff <= 2) return 2;
+      if (diff <= 4) return 4;
+      if (diff <= 8) return 8;
     }
 
     var findImage = function () {
